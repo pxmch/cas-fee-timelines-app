c510a14c190a22d9d95de9d1739a6f8a8ae9a062
Patrick <bitbucket@yum.ch>
2016-12-05 22:09:59 +0100

    implementing first timeline component tests

--- src/app/timeline/timeline.component.spec.ts
+++ src/app/timeline/timeline.component.spec.ts
@@ -1,9 +1,13 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ActivatedRoute} from '@angular/router';
+import {TimelinesService} from "../shared/model/timelines.service";
+import {Pipe} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {By} from '@angular/platform-browser';
 
-import { TimelineComponent } from './timeline.component';
+import {TimelineComponent} from './timeline.component';
+import {Timeline} from "../shared/model/timeline";
 
 describe('TimelineComponent', () => {
   let component: TimelineComponent;
@@ -11,7 +15,11 @@ describe('TimelineComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TimelineComponent ]
+      declarations: [ TimelineComponent, DatePipeMock ],
+      providers: [
+        {provide: TimelinesService, useClass: TimelineServiceStub },
+        {provide: ActivatedRoute, useClass: RouterStub }
+      ]
     })
       .compileComponents();
   }));
@@ -19,6 +27,7 @@ describe('TimelineComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TimelineComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
@@ -26,9 +35,38 @@ describe('TimelineComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it("should do math", () => {
-    expect(1 + 1).toEqual(2);
-
-    expect(5).toBeGreaterThan(4);
+  it('metadata should contain the correct title', () => {
+    expect(fixture.debugElement.query(By.css('.timeline__metadata')).nativeElement.textContent).toContain('Test-Timeline');
   });
 });
+
+class TimelineServiceStub {
+  public getTimelineByKey(id) {
+    return new Observable(observer => {
+      observer.next(new Timeline('ABDC', 'Test-Timeline', '', new Date(), true, new Date()));
+      observer.complete();
+    });
+  }
+  public getEventsForTimeline(id) { return null; }
+}
+
+class RouterStub {
+  public snapshot = { params: { id : 'ABCD'}}
+}
+
+/**
+ * This mock is needed because the angular 2 built in date pipe
+ * throws 'ReferenceError: Can't find variable: Intl' when running
+ * tests with karma
+ */
+@Pipe({
+  name: 'date',
+  pure: false // required to update the value when the promise is resolved
+})
+export class DatePipeMock implements Pipe {
+  name: string = 'date';
+
+  transform(query: string, ...args: any[]): any {
+    return query;
+  }
+}
