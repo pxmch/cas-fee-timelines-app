a8d0ad700b42bb647c37bf88f810f8109af3a609
Patrick <bitbucket@yum.ch>
2016-12-10 02:04:05 +0100

    homepage page improved; added content to about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
 
-
 @Component({
     selector: 'about',
-    templateUrl: '/about.component.html'
+    templateUrl: '/about.component.html',
+    styles: [
+      'i { text-transform: uppercase; font-style: normal; font-weight: 300; letter-spacing: 4px }',
+      'p { margin: 0 0 6px 0 }'
+    ]
 })
 
 export class AboutComponent {
