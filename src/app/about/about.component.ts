0789e2888bc2db509033d4af12c92c7ae0cd5e6a
Patrick <bitbucket@yum.ch>
2016-12-14 12:12:39 +0100

    adding some content to the about page for the presentation (+ some minor styling)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,7 +5,8 @@ import {Component} from '@angular/core';
     templateUrl: '/about.component.html',
     styles: [
       'i { text-transform: uppercase; font-style: normal; font-weight: 300; letter-spacing: 4px }',
-      'p { margin: 0 0 6px 0 }'
+      'p { margin: 0 0 6px 0; font: 16px/22px }',
+      'li { font: 16px/22px }'
     ]
 })
 
